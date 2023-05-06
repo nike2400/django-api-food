18c3cf4bc1fee4fdec10d85d8aea30fc1d7db739
nishi-2400 <a7w210872424bump@gmail.com>
2023-05-06 14:06:57 +1000

    check how it works

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Simple Unit Test
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+  
+  def test_add_numbers(self):
+    res = calc.add(5, 6)
+
+    self.assertEqual(res, 11)
+
+  def test_subtract_numbers(self):
+    res = calc.subtract(11, 5)
+
+    self.assertEqual(res, 6)
