7e3279af89be2ab5e65258bb715682b88adc0cf2
nishi-2400 <a7w210872424bump@gmail.com>
2023-06-09 12:25:58 +1000

    Linting

--- app/core/migrations/0002_auto_20230603_0118.py
+++ app/core/migrations/0002_auto_20230603_0118.py
@@ -45,7 +45,8 @@ class Migration(migrations.Migration):
                 ('price', models.DecimalField(decimal_places=2, max_digits=5)),
                 ('link', models.CharField(blank=True, max_length=255)),
                 ('user', models.ForeignKey(
-                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
